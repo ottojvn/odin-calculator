6396f38664c95c09b58c326f9150f0711ac08aad
Joao Victor Otto <ottojvn@gmail.com>
2022-08-26 15:41:04 -0300

    Add functionality to number buttons

--- script.js
+++ script.js
@@ -1,17 +1,62 @@
-function operate(operator, firstOperand, secondOperand) {
-  const validOperators = {
-    "/": () => firstOperand / secondOperand,
-    "*": () => firstOperand * secondOperand,
-    "-": () => firstOperand - secondOperand,
-    "+": () => firstOperand + secondOperand,
+function operate(operator, num1, num2) {
+  const operations = {
+    "/": () => num1 / num2,
+    "*": () => num1 * num2,
+    "-": () => num1 - num2,
+    "+": () => num1 + num2,
   };
 
-  const operation = validOperators[operator];
-
-  if (!operation) {
-    return null;
-  }
-
-  return operation();
+  const result = operations[operator]();
+  console.log(result);
 }
 
+function setupButtons() {
+  const numberButtons = document.querySelectorAll(".number");
+  numberButtons.forEach((button) =>
+    button.addEventListener("click", (event) =>
+      updateDisplay(event.target.value)
+    )
+  );
+
+  const operationButtons = document.querySelectorAll(".operation");
+  operationButtons.forEach((button) =>
+    button.addEventListener("click", collect)
+  );
+
+  const clearButton = document.querySelector("#clear");
+  clearButton.addEventListener("click", reset);
+}
+
+function collect() {
+  const displayNumber = parseFloat(displayText);
+  if (!firstOperand) {
+    firstOperand = displayNumber;
+    resetDisplay();
+  } else {
+    secondOperand = displayNumber;
+    resetDisplay();
+  }
+
+  operator = this.value;
+}
+
+function updateDisplay(text) {
+  display.textContent += text;
+}
+
+function resetDisplay() {
+  display.textContent = "";
+}
+
+function reset() {
+  firstOperand = null;
+  secondOperand = null;
+  operator = null;
+  resetDisplay();
+}
+
+let firstOperand;
+let secondOperand;
+let operator;
+const display = document.querySelector("#display");
+setupButtons();
